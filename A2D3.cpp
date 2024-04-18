f91824a780fed6b6a2aaa705f6b02d409177e2e8
Lam <lam@MacBook-Pro-4.local>
2024-04-18 18:53:33 +0300

    .

---
+++ A2D3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+#include <tuple>
+#define f0(i, n) for (int i = 0; i < (n); i++)
+#define f1(i, n) for (int i = 1; i <= (n); i++)
+#define f2(i, n) for (int i = 2; i <= (n); i++)
+#define mp make_pair
+
+using namespace std;
+typedef long long ll;
+const int mod = 1000000007;
+
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+
+    int n; cin >> n;
+
+    int res = 0;
+    f0(i, n){
+        int sum = 0;
+        f0(j, n){
+            int x; cin >> x;
+            sum += x;
+        }
+        res = max(res, sum);
+    }
+    cout << res;
+}

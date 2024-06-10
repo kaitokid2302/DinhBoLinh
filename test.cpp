ad4628a7eb20666c8917d61e7b64d77d11ec625e
basketball is my life <mra2322001@gmail.com>
2024-06-11 00:46:49 +0300

    .

---
+++ test.cpp
@@ -0,0 +1,12 @@
+// hello world
+
+#include <iostream>
+#define f0(i, n) for (int i = 0; i < n; i++)
+#define f1(i, n) for (int i = 1; i <= n; i++)
+
+using namespace std;
+
+int main() {
+    cout << "hello world" << endl;
+    return 0;
+}
